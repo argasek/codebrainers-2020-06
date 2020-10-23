5c9855ba71b5bc8258466a0aa8dc5110504e89da
Szymon Heller <hellerszymon70@gmail.com>
2020-10-23 17:56:40 +0200

    Homework - first task

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+// TASK 1
+
 let age = prompt('How old are you?');
-let parsedAge = parseInt(age);
-console.log(age, parsedAge);
-if (isNaN(parsedAge)) {
-    alert("Wpisz liczbę!! ");
+
+/*  - changed parseInt to parseFloat 
+    - parseInt would immidiately change our number into integer
+    - we left the possibility of input number being float
+*/
+let inputAge = parseFloat(age)
+
+console.log("Age = " + inputAge);
+
+if (isNaN(inputAge)) {
+    alert("Type in a number!");
+    // both 0 and negative numbers included
+} else if (inputAge <= 0) {
+    alert("You are probably a bit older.");
+} else if (inputAge >= 140) {
+    alert("You are probably a bit younger.")
+} else if (Number.isInteger(inputAge) === false) {
+    alert("Whole age was expected.")
 } else {
-    age = parsedAge + 13;
-    alert("Your age is " + age);
+    /* +13 added to final result as it was made during last class */
+    inputAge = inputAge + 13;
+    alert("Your age after adding 13 is " + inputAge + ".");
 }
+
+
