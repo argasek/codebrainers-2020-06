6092fb641d551526b94385373ecae8ede700c35c
Ziółkowska Alicja <alicjaziolkowska1986@wp.pl>
2020-10-24 16:23:18 +0200

    Read last character of the string

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const someString = 'Ala ma kota, kot ma mysz.';
+console.log(someString[someString.length-1]);
+// for (let i = 0; i < someString.length; i++) {
+//   console.log(someString[i]);
+//
+// }
 
-for (let i = 0; i < someString.length; i++) {
-  console.log(someString[i]);
-}
 
