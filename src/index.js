8fcbe4e6771d7b95be6fc505e3a4040e904eda4d
Szymon Heller <hellerszymon70@gmail.com>
2020-10-27 00:13:17 +0100

    Homework - task one

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@
 function drawNumber(n) {
   return Math.floor(Math.random() * n) + 1;
 }
+
+
+// TASK 1
+  const range = 49;
+  const outcome = [];
+
+  for (let i = 0; i < 6; i++) {
+    const result = drawNumber(range);
+    outcome.push(result);
+  }
+  alert("Randomization results: " + outcome + ".");
