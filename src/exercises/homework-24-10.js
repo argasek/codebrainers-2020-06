0ce059c8f2624cde4c06d31056bf7c348cd171b3
rafal_dub <rafaldud@gmail.com>
2020-10-26 18:28:12 +0100

    2nd Task

--- src/exercises/homework-24-10.js
+++ src/exercises/homework-24-10.js
@@ -1,10 +1,31 @@
+function isRepeated (arrayOfNumbers, number) {
+let repeated = false;
+for(let i = 0; i < arrayOfNumbers.length; i++) {
+if (arrayOfNumbers[i] === number) {
+repeated = true;
+}
+}
+return repeated
+}
+
+
 function generatingRandomArray(range, quantity) {
-    const randomArray = [];
-    for (let i = 0; i < quantity; i++) {
-    const singleNumber =  Math.floor(Math.random() * range) + 1;
-    randomArray.push(singleNumber);
+    if (quantity > range) {
+        return "You can't play";
     }
-    return randomArray;
-  }
+    else {
+        const randomArray = [];
+        let i = 0;
+        while (i < quantity) {
+            const singleNumber =  Math.floor(Math.random() * range) + 1;
+            if (!isRepeated(randomArray, singleNumber)) {
+                randomArray.push(singleNumber);
+                i++;
+            }
+        }
+        return randomArray;
+    }
+}
+
 const result = generatingRandomArray(49, 6);
-console.log(result)
+console.log(result);
