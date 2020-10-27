5275285bb3e6b0a33bfc635dedaf23a089017f7a
Szymon Tylmanowski <smile.tylmanowski@gmail.com>
2020-10-27 16:21:48 +0100

    Exercise-02 for Homework 24.10.2020

--- src/exercises/homework-24-10.js
+++ src/exercises/homework-24-10.js
@@ -1,14 +1,17 @@
 // ---------------------------
-// Exercise 01
+// Exercise 02
 // ---------------------------
 
 function drawNumber() {
     let numberArray = [];
     for(let i = 0; i < 6; i++ ){
         let number = Math.floor(Math.random()*49+1);
-        numberArray.push(number); 
+        if(numberArray.indexOf(number) === -1){
+            numberArray.push(number); 
+        }else{
+            i--;
+        }
     }
     console.log(numberArray);
 }
-
 drawNumber(49)
