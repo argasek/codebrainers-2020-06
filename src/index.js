76b0a99d62cf0ae545ea8ef8bfd9a79c66a6c125
Grzegorz Koczanowicz <grzegorzkoczanowicz@gmail.com>
2020-10-27 17:41:52 +0100

    Create Lottery Generator Number - Homework03

--- src/index.js
+++ src/index.js
@@ -2,3 +2,38 @@
 function drawNumber(n) {
   return Math.floor(Math.random() * n) + 1;
 }
+
+function lotto() {
+  const range = 49;
+  const a = drawNumber(range);
+  console.log(a);
+}
+
+function lottoTable() {
+    for (let i = 0; i < 6; i++) {
+    lotto();
+  }
+}
+
+let table = new Array(6);
+// table.push(lottoTable());
+table[0] = `${lottoTable()}`;
+
+console.log(table);
+
+// const table = [];
+//
+// function lotto() {
+//   if (table.length === 6) return;
+//   const range = 49;
+//   const a = drawNumber(range);
+//   for (let i = 0; i < table.length; i++) {
+//     if (a === table[1]){
+//       return lotto()
+//     }
+//   }
+//   table.push(a);
+// }
+//
+// console.log(table);
+
