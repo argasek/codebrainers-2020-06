f0c02ad63568c66880e56a67216be06271516ef1
rafal_dub <rafaldud@gmail.com>
2020-10-28 17:53:33 +0100

    first task

---
+++ src/exercises/exercise-04-homework.js
@@ -0,0 +1,4 @@
+const ArrayOfStrings= ["Kros", "mrok", "Mikro", "alf", "AAAAA", "aAAAaaa", "ZZZZZ", "bFDFDF"];
+const newArray = ArrayOfStrings.sort();
+console.log(newArray); // alphabetically capital letters, alphabetically lowercase letters
+
