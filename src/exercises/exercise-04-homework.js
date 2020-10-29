dc016f878efc80a9da76248f11cce4ead5b2f7c8
rafal_dub <rafaldud@gmail.com>
2020-10-29 12:30:28 +0100

    Second task

--- src/exercises/exercise-04-homework.js
+++ src/exercises/exercise-04-homework.js
@@ -1,4 +1,24 @@
-const ArrayOfStrings= ["Kros", "mrok", "Mikro", "alf", "AAAAA", "aAAAaaa", "ZZZZZ", "bFDFDF"];
-const newArray = ArrayOfStrings.sort();
-console.log(newArray); // alphabetically capital letters, alphabetically lowercase letters
+// const ArrayOfStrings= ["Kros", "mrok", "Mikro", "alf", "AAAAA", "aAAAaaa", "ZZZZZ", "bFDFDF"];
+// const newArray = ArrayOfStrings.sort();
+// console.log(newArray); // alphabetically capital letters, alphabetically lowercase letters
 
+function SameDrawnNumbers (firstArray, secondArray) {
+    const finalArray = [];
+    while (firstArray.length && secondArray.length) {
+        if (firstArray[0] === secondArray[0]){
+            finalArray.push(firstArray[0]);
+            firstArray.splice(0,1);
+            secondArray.splice(0,1);
+        }
+        else if (firstArray[0] > secondArray[0]) {
+            secondArray.splice(0,1);
+        }
+        else if (secondArray[0] > firstArray[0]) {
+            firstArray.splice(0,1);
+        }
+    }
+    return finalArray;
+}
+const firstExampleArray = [3, 8, 11, 14, 16, 22];
+const secondExampleArray = [3, 8, 17, 20, 21, 22];
+console.log(SameDrawnNumbers(firstExampleArray, secondExampleArray));
