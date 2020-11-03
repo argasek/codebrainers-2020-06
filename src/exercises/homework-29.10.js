8fcc70b47338ce3de08beefbe06c0a1c795f55b4
rafal_dub <rafaldud@gmail.com>
2020-11-03 09:23:05 +0100

    Homework second task

--- src/exercises/homework-29.10.js
+++ src/exercises/homework-29.10.js
@@ -1,9 +1,16 @@
-function intersection (arr1, arr2) {
-    const interArray = arr1.filter(function filterCommon (element) {
-        return arr2.includes(element);
-    })
-    return interArray
+// function intersection (arr1, arr2) {
+//     const interArray = arr1.filter(function filterCommon (element) {
+//         return arr2.includes(element);
+//     })
+//     return interArray
+// }
+// const a = [15, 22, 7, 49, 3, 2];
+// const b = [15, 7, 22, 18, 38, 2];
+// console.log(intersection(a, b));
+
+
+function realNumbersSolutions (arr) {
+    const delta = arr[1]**2 - 4*(arr[0]*arr[2]);
+    return delta >= 0;
 }
-const a = [15, 22, 7, 49, 3, 2];
-const b = [15, 7, 22, 18, 38, 2];
-console.log(intersection(a, b));
+console.log(realNumbersSolutions([15, 2, -3]))
