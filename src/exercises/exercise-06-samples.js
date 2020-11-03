d5cc72af416ca87d35292e339cc6e29fc95fbead
Grzegorz Koczanowicz <grzegorzkoczanowicz@gmail.com>
2020-11-03 20:55:19 +0100

    Create next sample: the function that return the biggest number

--- src/exercises/exercise-06-samples.js
+++ src/exercises/exercise-06-samples.js
@@ -1,13 +1,13 @@
-const points = [[5, 8, 1], [10, 20, 1]];
-const a = points[0];
-const b = points[1];
-const x = b[0] - a[0]
-const y = b[1] - a[1]
-const z = b[2] - a[2]
+// const points = [[5, 8, 1], [10, 20, 1]];
+// const a = points[0];
+// const b = points[1];
+// const x = b[0] - a[0]
+// const y = b[1] - a[1]
+// const z = b[2] - a[2]
 
-const avg = [a.reduce(function(acc, item, index, arr) {
-  return acc + item;
-})];
+// const avg = [a.reduce(function(acc, item, index, arr) {
+//   return acc + item;
+// })];
 
 
 // (x1, y1) (x2, y2)
@@ -15,15 +15,25 @@ const avg = [a.reduce(function(acc, item, index, arr) {
 // c = sqrt(a^2 + b^2)
 // c = sqrt((x2 - x1)^2 + (y2 - y1)^2 + )
 
-const lengthLine = points.reduce(function(acc, item, index, array){
-  console.log(`Acc: ${acc}`);
-  console.log(`item: ${item}`);
-  const x = (acc[0]-item[0]) * (acc[0]-item[0]);
-  const y = (acc[1]-item[1]) * (acc[1]-item[1]);
-  const z = (acc[2]-item[2]) * (acc[2]-item[2]);
-  const sum = x + y + z;
-  return Math.sqrt(sum);
-});
-console.log(lengthLine);
+// const lengthLine = points.reduce(function(acc, item, index, array){
+//   console.log(`Acc: ${acc}`);
+//   console.log(`item: ${item}`);
+//   const x = (acc[0]-item[0]) * (acc[0]-item[0]);
+//   const y = (acc[1]-item[1]) * (acc[1]-item[1]);
+//   const z = (acc[2]-item[2]) * (acc[2]-item[2]);
+//   const sum = x + y + z;
+//   return Math.sqrt(sum);
+// });
+// console.log(lengthLine);
+//
+// console.log(Math.sqrt(x*x + y*y + z*z));
 
-console.log(Math.sqrt(x*x + y*y + z*z));
+//  next samples: (najwieksza liczba)
+
+// const values = [6, 1, 13, 5, 20, 666, 113];
+//
+// const max = values.reduce(function (acc, item, index, arr) {
+//   console.log(acc, item);
+//   return Math.max(acc, item);
+// });
+// console.log(max);
