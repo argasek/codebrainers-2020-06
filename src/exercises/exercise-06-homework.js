5f9fd3747eea03c6730f4b12fcd4caa984ba99ce
Grzegorz Koczanowicz <grzegorzkoczanowicz@gmail.com>
2020-11-04 21:49:53 +0100

    Homework - first task

---
+++ src/exercises/exercise-06-homework.js
@@ -0,0 +1,36 @@
+// TASK 1.
+
+// const a = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+// console.log(a, a===1);
+
+// first sample:
+// let b;
+// b = Math.round((a*10)/10);
+// console.log(b, b===1);
+
+
+// second sample:
+// let c;
+// c = a;
+// c = c.toFixed(0)
+// console.log(c, c===1);
+
+// third sample:
+//
+// Number.prototype.round = function(item) {
+//     return +(Math.round(this+"e+"+item) + "e-"+item);
+// }
+// let x = 1.005
+// x = x.round(2);
+// console.log(x, x===1.01);
+
+// fourth sample:
+// let d=a;
+// d = d.toPrecision(1);
+// console.log(d, d===1);
+//
+// console.log(Math.ceil(a), Math.ceil(a)===1);
+
+
+// TASK 2.:
+
