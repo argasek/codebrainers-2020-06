20e94daf19e20e4beb7b34734d2f70441ae8e1cf
Grzegorz Koczanowicz <grzegorzkoczanowicz@gmail.com>
2020-11-04 21:52:25 +0100

    Homework - first task

---
+++ src/index.js
@@ -0,0 +1,51 @@
+// const points = [[5, 8, 1], [10, 20, 1]];
+// const a = points[0];
+// const b = points[1];
+// const x = b[0] - a[0]
+// const y = b[1] - a[1]
+// const z = b[2] - a[2]
+
+// const avg = [a.reduce(function(acc, item, index, arr) {
+//   return acc + item;
+// })];
+
+
+// // (x1, y1) (x2, y2)
+// // c^2 = a ^2 + b^2
+// // c = sqrt(a^2 + b^2)
+// // c = sqrt((x2 - x1)^2 + (y2 - y1)^2 + )
+
+// const lengthLine = points.reduce(function(acc, item, index, array){
+//   console.log(`Acc: ${acc}`);
+//   console.log(`item: ${item}`);
+//   const x = (acc[0]-item[0]) * (acc[0]-item[0]);
+//   const y = (acc[1]-item[1]) * (acc[1]-item[1]);
+//   const z = (acc[2]-item[2]) * (acc[2]-item[2]);
+//   const sum = x + y + z;
+//   return Math.sqrt(sum);
+// });
+// console.log(lengthLine);
+
+// console.log(Math.sqrt(x*x + y*y + z*z));
+
+
+const values = [6, 1, 13, 5, 20, 666, 113];
+const oddValues = values.filter(function(value){
+    return value % 2 !== 0;
+})
+
+const max = values.reduce(function(previousValue, currentValue){
+    console.log(previousValue, currentValue);
+
+    // return Math.max(currentValue, previousValue);
+})
+console.log(max);
+
+// const maxOdd = values.reduce(function(value){
+//     const oddValues = [];
+//     if (value % 2 !== 0) {
+//         oddValues.push(value);
+//     }
+//     console.log(oddValues);
+
+// });
