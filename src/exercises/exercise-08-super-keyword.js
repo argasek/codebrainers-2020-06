988bdf96c470441f7216eca10f4861ba0a9e64e5
Jakub Argasiński <jakub@argasinski.eu>
2020-11-07 13:44:27 +0100

    Exercise 08 - super keyword

---
+++ src/exercises/exercise-08-super-keyword.js
@@ -0,0 +1,62 @@
+class Lock {
+  constructor() {
+    this.isLocked = false;
+  }
+}
+
+class Door {
+  constructor() {
+    this.doorLock = new Lock();
+    this.type = undefined;
+  }
+
+  lock() {
+    this.doorLock.isLocked = true;
+  }
+
+  unlock() {
+    this.doorLock.isLocked = false;
+  }
+
+  isLocked() {
+    return this.doorLock.isLocked;
+  }
+}
+
+class MovingDoor extends Door {
+
+  constructor() {
+    super();
+    this.position = 'closed';
+  }
+
+  open() {
+    this.unlock();
+    this.position = 'opened';
+  }
+
+}
+
+class MovingDoorWithAdditionalLock extends MovingDoor {
+
+  constructor() {
+    super();
+    this.position = 'closed';
+    this.additionalLock = new Lock();
+    this.additionalLock.isLocked = true;
+  }
+
+  open() {
+    super.open();
+    this.additionalLock.isLocked = false;
+  }
+
+}
+
+
+
+const door = new Door();
+
+door.lock();
+
+console.log(door);
