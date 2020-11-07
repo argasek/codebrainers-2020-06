bae3c39b727d11283e8f398ca8148768ed22a3b8
rafal_dub <rafaldud@gmail.com>
2020-11-07 13:53:44 +0100

    Create Engine and Vehicle classes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-class Car {
+class Engine {
+    constructor (){
+        this.isRunning = false;
+        this.type = undefined;
+    }
+    turnOn (){
+        this.isRunning = true;
+    }
+    turnOff (){
+        this.isRunning = false;
+    }
+}
+class AirplaneEngine extends Engine {
+    constructor(){
+        super();
+        this.type = "airplaneEngine";
+    }
+}
+
+
+class Vehicle {
+    constructor(){
+        this.engines = [];
+
+}
+    addEngine (engine) {
+        this.engines.push(engine);
+    }
 
 }
