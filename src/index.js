1504f4551cb8c0889dbfc6ef48d71bb55b9ed1cc
rafal_dub <rafaldud@gmail.com>
2020-11-07 15:26:25 +0100

    Explain forEach execution context (i.e. global context versus object context)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 (function () {
     this.name = "Arrow function";
-
-    let o = {
-        name: "Regular function",
-        thisInRegular() {
-            console.log("Example of " + this.name);
-        },
-        thisInArrow: () => {
-            console.log("Example of " + this.name);
-        },
-    };
-
-    o.thisInRegular();
-    o.thisInArrow();
+    let o;
+    (function () {
+        o = {
+            items: [1, 2, 3, 4],
+            name: "Regular function",
+            thisInRegular() {
+                console.log("Example of " + this.name);
+            },
+            thisInArrow: () => {
+                console.log("Example of " + this.name);
+            },
+            applySquares: function () {
+                this.items.forEach(function (item){
+                    console.log(item);
+                    console.log(this.name);
+                }, this)
+            }
+        };
+    })();
+    
+    o.applySquares();
+    // o.thisInRegular();
+    // o.thisInArrow();
 })();
