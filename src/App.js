409d5eec92683e230d4141dade25112d440bf53a
Szymon Tylmanowski <smile.tylmanowski@gmail.com>
2020-11-19 07:33:42 -0800

    Exercise 17.11

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import './App.css';
 function App() {
   const arr = ['red', 'green', 'blue', 'black', 'maroon', 'teal', 'dadadass'];
   const styleObject = {
-    color: 'blue'
+    color: 'green',
+    backgroundColor: 'pink',
   };
 
-  const ListItem = props => <li style={props.style}>{props.text}: { props.color }</li>;
+const arrayOfColorParagraphs = arr.map((color, index) => index % 2 != 0 ? <li style={styleObject}>{color}</li> : <li>{' I am Groot ' + color}</li>);
 
-  const mapper = function (color, index) {
-    const isIndexEqual2 = index === 2;
-    const styledListItem = <ListItem style={styleObject} color={color} text="Special color" />;
-    const regularListItem = <li>Color name: { color }</li>;
-    return isIndexEqual2 ? styledListItem : regularListItem;
-  };
-
-  // 2
-  const arrayOfColorParagraphs = arr.map(mapper);
 
   return (
     <div>
