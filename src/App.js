46d84eefd1d67e3c52d070069277a5a96287d2da
Jakub Argasiński <jakub@argasinski.eu>
2020-11-21 09:20:11 +0100

    Exercise 11 - Class components, componentDidMount()

--- src/App.js
+++ src/App.js
@@ -2,36 +2,76 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const arr = ['red', 'green', 'blue', 'black', 'maroon', 'teal', 'dadadass'];
-  const styleObject = {
-    color: 'green',
-    backgroundColor: 'pink'
-  };
+class App extends React.PureComponent {
 
-  const ListItem = props => <li style={props.style}>{props.text}: { props.color }</li>;
+  componentDidMount() {
+    console.log('App was mounted');
+  }
 
-  const mapper = function (color, index) {
-    const isIndexEven = index % 2;
-    const evenListItem = <ListItem style={styleObject} color={color} text="Special color" />;
-    const oddListItem = <li color={color}>{`"I'm Groot!" Color name: ${color}`}</li>;
-    return isIndexEven ? evenListItem : oddListItem;
-  };
+  render() {
+    const arr = ['red', 'green', 'blue', 'black', 'maroon', 'teal', 'dadadass'];
+    const styleObject = {
+      color: 'green',
+      backgroundColor: 'pink'
+    };
 
-  // 2
-  const arrayOfColorParagraphs = arr.map(mapper);
+    const ListItem = props => <li style={props.style}>{props.text}: { props.color }</li>;
 
-  return (
-    <div>
-      <h1>Hej!</h1>
-      <p>Ala ma kota</p>
-      <ul>
-        {
-          arrayOfColorParagraphs
-        }
-      </ul>
-    </div>
-  );
+    const mapper = function (color, index) {
+      const isIndexEven = index % 2;
+      const evenListItem = <ListItem style={styleObject} color={color} text="Special color" />;
+      const oddListItem = <li color={color}>{`"I'm Groot!" Color name: ${color}`}</li>;
+      return isIndexEven ? evenListItem : oddListItem;
+    };
+
+    // 2
+    const arrayOfColorParagraphs = arr.map(mapper);
+
+    return (
+      <div>
+        <h1>Hej!</h1>
+        <p>Ala ma kota</p>
+        <ul>
+          {
+            arrayOfColorParagraphs
+          }
+        </ul>
+      </div>
+    );
+  }
 }
+
+
+// function App() {
+//   const arr = ['red', 'green', 'blue', 'black', 'maroon', 'teal', 'dadadass'];
+//   const styleObject = {
+//     color: 'green',
+//     backgroundColor: 'pink'
+//   };
+//
+//   const ListItem = props => <li style={props.style}>{props.text}: { props.color }</li>;
+//
+//   const mapper = function (color, index) {
+//     const isIndexEven = index % 2;
+//     const evenListItem = <ListItem style={styleObject} color={color} text="Special color" />;
+//     const oddListItem = <li color={color}>{`"I'm Groot!" Color name: ${color}`}</li>;
+//     return isIndexEven ? evenListItem : oddListItem;
+//   };
+//
+//   // 2
+//   const arrayOfColorParagraphs = arr.map(mapper);
+//
+//   return (
+//     <div>
+//       <h1>Hej!</h1>
+//       <p>Ala ma kota</p>
+//       <ul>
+//         {
+//           arrayOfColorParagraphs
+//         }
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default App;
