cdb451083945f661c356bb2f57f79e148d4a3fc4
Szymon Tylmanowski <smile.tylmanowski@gmail.com>
2020-11-21 02:38:35 -0800

    Use CSS styles to improve visual aspects of StudentsList table

--- src/components/studentsList.jsx
+++ src/components/studentsList.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import './studentsList.css';
 
 class StudentsList extends React.PureComponent {
+
+    render() {
+        return (
+            <table className='students-table'>
+                <tr>
+                    <td>
+                        dsadsa
+                    </td>
+                    <td>
+                        ddsadsa
+                    </td>
+                </tr>
+            </table>
+        )
+    }
 
 }
 
