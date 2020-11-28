34b6e3f8261159a4198ce15b5507471955454a1e
Twoje imię i nazwisko <twj@email>
2020-11-28 14:29:18 +0100

    Fetch plants from API

--- src/components/plants/Plants.jsx
+++ src/components/plants/Plants.jsx
@@ -2,21 +2,64 @@ import { Card, CardBody } from "reactstrap";
 import React from "react";
 import PropTypes from "prop-types";
 import PlantRow from "components/plants/Plant";
+import axios from "axios";
+import Plant from "models/plant";
 
-const requestUrl = "http://gentle-tor-07382.herokuapp.com/plants/";
+const PLANTS_FETCH_DELAY = 5000;
 
 class Plants extends React.PureComponent {
-
-  render() {
-    const plant = {
-      id: 1,
-      name: 'Some plant'
+  constructor(props) {
+    super(props)
+    this.state = {
+      inProgress: false,
+      successPlants: undefined,
+      plants: [new Plant()],
     };
+  }
 
+
+  componentDidMount() {
+    this.fetchPlants()
+      .finally(() => {
+        this.setState({ inProgress: false });
+      })
+  }
+
+  fetchPlants() {
+
+    const requestUrl = "http://gentle-tor-07382.herokuapp.com/plants/";
+    this.setState({ inProgress: true });
+    return this.props.delayFetch(PLANTS_FETCH_DELAY, (resolve, reject) => {
+      return axios.get(requestUrl)
+        .then((response) => {
+          const data = response.data;
+          console.log(data);
+          // const categories = data.map((item) => item.name);
+          const successPlants = true;
+          const plants = data.map((item)=> {
+            const plant = new Plant();
+            return  plant.fromPlain(item);
+          });
+          debugger;
+          this.setState({ plants, successPlants });
+          console.log('after setState();')
+          resolve(response);
+        })
+        .catch((error) => {
+          this.setState({ successPlants: false });
+          reject(error);
+        })
+        .finally(() => {
+          console.log('Resolved');
+        });
+    });
+  }
+  render() {
+    const {inProgress, successPlants, plants} = this.state;
     return (
       <Card className="mb-4">
         <CardBody>
-            <PlantRow plant={plant}  />
+            <PlantRow plant={plants[0]}  />
         </CardBody>
       </Card>
     );
---
+++ src/models/plant.js
@@ -0,0 +1,25 @@
+class Plant {
+  constructor() {
+    this.id = undefined;
+    this.name = "";
+  }
+  fromPlain (item) {
+    this.blooming = item.blooming;
+    this.category = item.category;
+    this.categorySlug = item.category_slug;
+    this.difficulty = item.difficulty;
+    this.fertilizingInterval = item.fertilizing_interval;
+    this.id = item.id;
+    this.lastFertilized = item.last_fertilized;
+    this.lastWatered = item.last_watered;
+    this.name = item.name;
+    this.requiredExposure = item.required_exposure;
+    this.requiredHumidity = item.required_humidity;
+    this.requiredTemperature = item.required_temperature;
+    this.room = item.room;
+    this.url = item.url;
+    this.wateringInterval = item.watering_interval;
+    return this
+  }
+}
+export default Plant;
