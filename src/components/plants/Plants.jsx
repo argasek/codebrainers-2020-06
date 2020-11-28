6b39e22ebcb242e58ece070fa0261223e90ee29c
Jakub Argasiński <jakub@argasinski.eu>
2020-11-28 13:26:26 +0100

    Exercise 14 - Fetching and transforming data (Plants) from API.

--- src/components/plants/Plants.jsx
+++ src/components/plants/Plants.jsx
@@ -1,66 +1,22 @@
 import { Card, CardBody } from "reactstrap";
 import React from "react";
 import PropTypes from "prop-types";
-import axios from "axios";
-import Plant from "components/plants/Plant";
-import InProgress from "components/shared/InProgress";
+import PlantRow from "components/plants/Plant";
 
-const PLANTS_FETCH_DELAY = 250;
+const requestUrl = "http://gentle-tor-07382.herokuapp.com/plants/";
 
 class Plants extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      plants: [],
-      successPlants: undefined,
-      inProgress: false,
-    };
-  }
-
-  componentDidMount() {
-    this.fetchPlants().finally(() => {
-      this.setState({ inProgress: false });
-    });
-  }
-
-  fetchPlants() {
-    const requestUrl = "http://gentle-tor-07382.herokuapp.com/plants/";
-    this.setState({ inProgress: true });
-    return this.props.delayFetch(PLANTS_FETCH_DELAY, (resolve, reject) => {
-      axios
-        .get(requestUrl)
-        .then((response) => {
-          const data = response.data;
-          const plants = data.map((item) => {
-            const { id, name } = item;
-            return { id, name };
-          });
-          const successPlants = true;
-          this.setState({ plants, successPlants });
-          resolve();
-        })
-        .catch((error) => {
-          this.setState({ successPlants: false });
-          reject();
-        });
-    });
-  }
 
   render() {
-    const { plants, successPlants, inProgress } = this.state;
+    const plant = {
+      id: 1,
+      name: 'Some plant'
+    };
 
     return (
       <Card className="mb-4">
         <CardBody>
-          <InProgress inProgress={inProgress} />
-          {successPlants === false && <p>Nie udało się pobrać Kwiatow</p>}
-          {successPlants && (
-            <div className="plants">
-              {plants.map((plant, index, arr) => (
-                <Plant plant={plant} key={index} />
-              ))}
-            </div>
-          )}
+            <PlantRow plant={plant}  />
         </CardBody>
       </Card>
     );
